d93bfdf517c91fa65a12ad67d1172fe7dd2eb7f2
Yago Oliveira <guito_flash@yahoo.com>
2024-09-09 15:26:37 -0300

    Conversor de posição para o xadrez real

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -1,4 +1,4 @@
-﻿using xadrez_console.Tabuleiro;
+﻿using xadrez_console.Tabuleiro.Xadrez;
 
 namespace xadrez_console;
 
@@ -6,21 +6,26 @@ class Program
 {
     static void Main(string[] args)
     {
+        PosicaoXadrez pos = new('a', 1);
+        PosicaoXadrez pos2 = new('c', 7);
 
-        try
-        {
-            Tabuleiros tab = new(8, 8);
+        Console.WriteLine(pos);
+        Console.WriteLine(pos.ToPosicao());
+        Console.WriteLine(pos2.ToPosicao());
+        //try
+        //{
+        //    Tabuleiros tab = new(8, 8);
 
-            tab.ColocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
-            tab.ColocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
-            tab.ColocarPeca(new Rei(tab, Cor.Preta), new Posicao(2, 4));
-            tab.ColocarPeca(new Rei(tab, Cor.Preta), new Posicao(0, 2));
+        //    tab.ColocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
+        //    tab.ColocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
+        //    tab.ColocarPeca(new Rei(tab, Cor.Preta), new Posicao(2, 4));
+        //    tab.ColocarPeca(new Rei(tab, Cor.Preta), new Posicao(0, 2));
 
-            Tela.ImprimirTabuleiro(tab);
-        }
-        catch (Exception e)
-        {
-            Console.WriteLine(e.Message);
-        }
+        //    Tela.ImprimirTabuleiro(tab);
+        //}
+        //catch (Exception e)
+        //{
+        //    Console.WriteLine(e.Message);
+        //}
     }
 }
